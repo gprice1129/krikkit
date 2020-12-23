1fe353b7466d8598297b2d6f466691bbc6388850
Guthrie Price <gprice1129@gmail.com>
2020-12-22 17:44:39 -0800

    cleanup add todo

--- fish_pumpkin_server.rkt
+++ fish_pumpkin_server.rkt
@@ -1,36 +1,15 @@
 #lang racket/base
 (require racket/pretty "fish_pumpkin_state.rkt" "network.rkt")
 (module+ main
-  ;(define (handle-events st)
-    ;(foldl
-      ;(lambda (event st)
-        ;(define as (event->actions event))
-        ;(handle-actions st as))
-      ;st
-      ;(evq 'pop)))
-
-  ;(define (replayer actions)
-    ;(lambda (st)
-      ;(if (null? actions)
-          ;st
-          ;(let ()
-            ;(define a (car actions))
-            ;(set! actions (cdr actions))
-            ;(sleep 0.1)
-            ;(st 'handle-action a)))))
-
-  ;(define (stop? st) (st 'winning?))
-
-  ;(void (thread (lambda ()
-          ;(define st.init (sokoban-state map.w1))
-          ;(define actions ((game-loop st.init handle-events world-present! stop?) 'history))
-          ;(define replay-action! (replayer actions))
-          ;(pretty-print `(you won! using these actions: ,actions))
-          ;(game-loop st.init replay-action! world-present! stop?)
-          ;(pretty-print '(replay over hope you liked it!))))))
+    ; TODO
+    ; player specific actions
+    ; assume no erros (for now)
+    ; send/get actions
+    ; check for new connections
+    ; remove quitters
     (define listener (tcp:listener 8124))
-    ((((let loop ((connections '()))
+    (let loop ((connections '()))
         (for-each (lambda (conn)
             (conn 'write "New player joined")) connections)
-        (loop (append (list (listener 'accept)) connections)))))))
+        (loop (append (list (listener 'accept)) connections))))
         
